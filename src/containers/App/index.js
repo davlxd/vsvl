81fc903a3b7fae34bfe42479ad2521cda140c8bf
lxd <i@lxd.me>
2018-11-19 19:34:12 +1300

    Involve material-ui

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-
 import { connect } from 'react-redux'
+
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 import logo from './logo.svg'
 import './index.css'
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <CssBaseline />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
